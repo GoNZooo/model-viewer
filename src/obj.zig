89079e1c6ddff9c21d2e5c6c90e4d5d30960f815
Rickard Andersson <gonz@severnatazvezda.com>
2019-12-15 12:18:29 +0200

    add data to `Index3`

--- src/obj.zig
+++ src/obj.zig
@@ -8,7 +8,9 @@ pub fn Vertex(comptime size: comptime_int, comptime T: type) type {
     };
 }
 
-pub const Index3 = struct {};
+pub const Index3 = struct {
+    data: [3][3]usize,
+};
 
 pub const Obj = struct {
     vertices: []Vertex(3, f32),
