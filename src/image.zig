87ff59d20a258496c2807b44512341e118d97222
Rickard Andersson <gonz@severnatazvezda.com>
2020-04-29 14:35:08 +0300

    fix  data slicing when creating `Image`
    Using an extra allocation right now to more conveniently take ownership.
    
    Probably something to look at streamlining if `Image`s are being created often.

--- src/image.zig
+++ src/image.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 const mem = std.mem;
 const debug = std.debug;
 const testing = std.testing;
+const heap = std.heap;
 
 const c = @cImport({
     @cInclude("stb_image.h");
@@ -15,27 +16,33 @@ pub const Image = struct {
     channels: u32,
     data: []const u8,
 
-    pub fn fromPath(path: []const u8) !Image {
+    pub fn fromPath(allocator: *mem.Allocator, path: []const u8) !Image {
         var width: c_int = undefined;
         var height: c_int = undefined;
         var channels: c_int = undefined;
-        const data = c.stbi_load(path.ptr, &width, &height, &channels, 0);
+        const raw_data = c.stbi_load(path.ptr, &width, &height, &channels, 0);
+        if (raw_data == null) return error.UnableToLoadImage;
+        defer c.stbi_image_free(raw_data);
 
-        if (data == null) return error.UnableToLoadImage;
+        const needed_bytes = @intCast(usize, width * height * channels);
+        var data = try allocator.alloc(u8, needed_bytes);
+        for (data) |_, i| {
+            data[i] = raw_data[i];
+        }
 
         return Self{
             .width = @intCast(u32, width),
             .height = @intCast(u32, height),
             .channels = @intCast(u32, channels),
-            .data = mem.span(data),
+            .data = data,
         };
     }
 };
 
 test "load 'hello.png'" {
-    const hello = try Image.fromPath("./res/textures/hello.png");
-    testing.expectEqual(hello.data.len, 437874);
+    const hello = try Image.fromPath(heap.page_allocator, "./res/textures/hello.png");
     testing.expectEqual(hello.width, 497);
     testing.expectEqual(hello.height, 454);
     testing.expectEqual(hello.channels, 4);
+    testing.expectEqual(hello.data.len, 902552);
 }
